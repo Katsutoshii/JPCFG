dc73f19b981e68dc1dfc9af6347f2a069e5eaf54
Josiah Putman <joshikatsu@gmail.com>
2019-05-29 11:37:54 -0400

    Formatting

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ Project: src
 File Created: Wednesday, 22nd May 2019 11:04:39 am
 Author: Josiah Putman (joshikatsu@gmail.com)
 -----
-Last Modified: Wednesday, 29th May 2019 11:36:58 am
+Last Modified: Wednesday, 29th May 2019 11:37:47 am
 Modified By: Josiah Putman (joshikatsu@gmail.com)
 '''
 
 from dirs import TREEBANKD
 from ktbparse import KTBParser
 from larkadapter import LarkAdapter
- 
+
 if __name__ == "__main__":
     ktbparser = KTBParser()
     ktbparser.parse(TREEBANKD / 'aozora_Akutagawa-1922.psd')
