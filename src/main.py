416efdb00105eff45735efb04a4f90c570feed7f
Josiah Putman <josiah.k.putman.20@dartmouth.edu>
2019-05-29 16:19:48 -0400

    Earley parsing

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ Project: src
 File Created: Wednesday, 22nd May 2019 11:04:39 am
 Author: Josiah Putman (joshikatsu@gmail.com)
 -----
-Last Modified: Monday, 27th May 2019 1:27:19 am
+Last Modified: Tuesday, 28th May 2019 9:33:44 pm
 Modified By: Josiah Putman (joshikatsu@gmail.com)
 '''
 
@@ -14,5 +14,6 @@ from ktbparse import KTBParser
 if __name__ == "__main__":
     ktbparser = KTBParser()
     ktbparser.parse(TREEBANKD / 'aozora_Akutagawa-1922.psd')
+    print(ktbparser.pcfg.prods('PP'))
     print("Done.")
     
--- src/pcfg.py
+++ src/pcfg.py
@@ -4,14 +4,27 @@ Project: src
 File Created: Wednesday, 22nd May 2019 11:04:27 am
 Author: Josiah Putman (joshikatsu@gmail.com)
 -----
-Last Modified: Sunday, 26th May 2019 11:34:21 pm
+Last Modified: Tuesday, 28th May 2019 10:57:13 pm
 Modified By: Josiah Putman (joshikatsu@gmail.com)
 '''
-from typing import Dict, List
+from typing import Dict, List, Set
 from collections import defaultdict, namedtuple
 
 Rule = namedtuple("Rule", ["lhs", "rhs"])
 
 class PCFG():
     def __init__(self):
-        self.counts: Dict[Rule, int] = defaultdict(int)
+        self.rules: Dict[str, Set[Rule]] = defaultdict(set)         # symbols to their rules
+        self.counts: Dict[Rule, int] = defaultdict(int)             # rules to their counts
+        self.preterminals: Dict[str, Set[str]] = defaultdict(set)   # terminals to preterminals
+        self.starts = set()                                         # all possible start variables
+
+    def add(self, rule: Rule) -> None:
+        self.counts[rule] += 1
+        self.rules[rule.lhs].add(rule)
+
+    def add_terminal(self, rule: Rule) -> None:
+        self.preterminals[rule.rhs[0]].add(rule.lhs)
+
+    def prods(self, lhs: str) -> Set[Rule]:
+        return self.rules[lhs]
